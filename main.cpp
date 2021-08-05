ef01bfcb95eb15af34c6cd719ad58dfe9308bfb2
thennys <thennyswhyte@gmail.com>
2021-08-05 02:49:43 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+   // cout << "Hello Top Whyte ! please enter your test scores :!" << endl;
+    //arrays
+   // char myCourses [7] {"com skills ", "math" , "frnc", "eng lab","tD" , "circuit Design","socialStudies"};
+   // int testScores [7]{0,1,2,};
+    //cout <<"enter score for com skills"<<endl;
+    //cin >>testScores[0];
+     //cout <<"enter score for frnc"<<endl;
+    //cin >>testScores[1];
+     //cout <<"enter score for eng lab"<<endl;
+    //cin >>testScores[2];
+     //cout <<"enter score for td"<<endl;
+    //cin >>testScores[3];
+     //cout <<"enter score for circuit design"<<endl;
+    //cin >>testScores[4];
+     //cout <<"enter score for socialStudies"<<endl;
+    //cin >>testScores[5];
+    //cout <<"enter score for math"<<endl;
+    //cin >>testScores[6];
+    //cout << "your testScores are :" <<endl;
+    //cout << testScores[0]<< endl;
+    //cout << testScores[1]<< endl;
+     //cout << testScores[2]<< endl;
+      //cout << testScores[3]<< endl;
+       //cout << testScores[4]<< endl;
+        //cout << testScores[5]<< endl;
+         //cout << testScores[6]<< endl;
+
+
+
+
+   //cout << "Hello Thennys please enter your test scores!" << endl;
+    //vectorsFunctions
+   // char myCourses [7] {"com skills ", "math" , "frnc", "eng lab","tD" , "circuit Design","socialStudies"};
+ //vector <int> testScores(90);
+    //cout <<"enter score for com skills"<<endl;
+    //cin >>testScores[0];
+     //cout <<"enter score for frnc"<<endl;
+    //cin >>testScores.at(1);
+     //cout <<"enter score for eng lab"<<endl;
+    //cin >>testScores.at(2);
+     //cout <<"enter score for td"<<endl;
+    //cin >>testScores[3];
+     //cout <<"enter score for circuit design"<<endl;
+   // cin >>testScores[4];
+    // cout <<"enter score for socialStudies"<<endl;
+    //cin >>testScores.at(5);
+    //cout <<"enter score for math"<<endl;
+    //cin >>testScores[6];
+    //cout << "your testScores are :" <<endl;
+    //cout << testScores[0]<< endl;
+    //cout << testScores[1]<< endl;
+     //cout << testScores[2]<< endl;
+      //cout << testScores[3]<< endl;
+       //cout << testScores[4]<< endl;
+        //cout << testScores[5]<< endl;
+         //cout << testScores[6]<< endl;
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
